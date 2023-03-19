d63e1c9eb255773049d20f70a0f56f7daab069fd
martindwyer <martin@mdbytes.com>
2023-03-19 14:32:47 -0500

    Refactoring

--- src/components/layout/CookiesApproval.tsx
+++ src/components/layout/CookiesApproval.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const CookiesApproval = () => {
   const [cookiesApproved, setCookiesApproved] = useState(false);
